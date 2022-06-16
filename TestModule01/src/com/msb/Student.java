6ba74b85fa940b21418a0bb1eecc66d425898c73
Li Lei <noblebrad1@126.com>
2022-06-16 14:37:21 +0800

    第一次进行数据的提交

---
+++ TestModule01/src/com/msb/Student.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: Li Lei
+ * @Date: 2022/6/16 - 14:35
+ * @Description: TestGit
+ * @version: 1.0
+ */
+public class Student {
+    private int age;
+    private String name;
+}
